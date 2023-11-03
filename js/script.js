cd830185ce55c69173062c89f9612d9c5df15e01
Tomasz Piotrowski <tpiotrowski007@gmail.com>
2023-11-03 02:51:22 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+let heightElement = document.querySelector(".js-height");
+let weightElement = document.querySelector(".js-weight");
+let formElement = document.querySelector(".js-form");
+let bmiElement = document.querySelector(".js-bmi");
+
+heightElement.addEventListener("input", () => {
+    console.log(`Aktualny wzrost: ${heightElement.value} cm`);
+});
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+    let height = heightElement.value;
+    let weight = weightElement.value;
+    let bmi = weight / ((height / 100) ** 2);
+    console.log(bmi);
+    bmiElement.innerText = bmi.toFixed(2);
+
+
+});
+formElement.addEventListener("reset", (event) => {
+    console.log("Formularz został zresetowany");
+});
